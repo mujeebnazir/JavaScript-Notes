8e317f770b0d793d9499c07de145e1ec2136361d
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2023-08-26 15:41:02 +0000

    strings

---
+++ 01-basics/strings.js
@@ -0,0 +1,29 @@
+const name = "Mujeeb"
+const repoCount = 50 
+
+//console.log(name + repoCount + " value")
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(4));
+// console.log(gamename.indexOf('j'));
+// const newStr = gamename.substring(0,4)
+// console.log(newStr)
+// const anotherStr = gamename.slice(-4,4)//negative can be used
+// console.log(anotherStr)
+
+const newStringOne = "  mujeeb  "
+console.log(newStringOne)
+console.log(newStringOne.trim())//removes begning spaces and endspaces
+
+const url = "https://mujeeb.com/mujeeb%20nazir";
+console.log(url.replace("%20",'-'))
+.includes
+
+
+
+
