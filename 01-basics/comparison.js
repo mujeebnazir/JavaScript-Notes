2e734e30a7da9a6fd25192ae98070be06ceb6bd9
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2023-08-26 10:20:47 +0000

    comparison

---
+++ 01-basics/comparison.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02">1);
+
+// // console.log(null > 0) //false
+// // console.log(null == 0) //false
+// // console.log(null >= 0) //true
+
+//avoid these two thing
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log (undefined <0 )
+
+//strict check === 
