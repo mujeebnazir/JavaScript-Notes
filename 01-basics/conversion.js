e874c7940910e0185cbcb614fd3c058d64b52111
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2023-08-26 10:00:21 +0000

    conversions

---
+++ 01-basics/conversion.js
@@ -0,0 +1,53 @@
+// let score = "33abd"
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber  = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN
+
+// let score = null
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber  = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  //0
+
+// let score = undefined
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber  = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN
+
+
+// let score = true
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber  = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //1
+
+//"33"=>33
+//"33bsc" => NaN
+// true =>1;false =>0
+
+// let isLoggedIn = "mujeeb"
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+//1=> true; 0 =>false
+// "hitesh" => true; "" => false
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber) 
+console.log(typeof stringNumber) 
