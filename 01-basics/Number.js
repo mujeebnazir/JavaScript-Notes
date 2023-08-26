bb0faeb9ab2134add5f749cf6668fc7db117c09d
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2023-08-26 16:26:41 +0000

    Math and numbers

---
+++ 01-basics/Number.js
@@ -0,0 +1,35 @@
+// const score = 300 
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 1123.9855
+// console.log(otherNumber.toPrecision(4))
+
+
+// *********************************************
+
+// console.log(Math)
+// console.log(Math.abs(4.5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(2,4,56,6,));
+
+console.log(Math.random())//1-0
+console.log((Math.random()*10000)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min +1))+ min);//imp
+
+
+
+
+
+
