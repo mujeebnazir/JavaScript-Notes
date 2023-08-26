2fdfe3d55b38d48531fe38bc3b493fe4664712d6
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2023-08-26 10:42:04 +0000

    memory usage of datatypes

--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -7,3 +7,22 @@
 //Array,Objects, Functions
 
 //js is dynamic not static language
+
+//&*********************************************************************
+//stack(primitive),heap(non - primitive)
+// let myYoutubename = "mujeebnazir.com";
+// let anothername = myYoutubename;
+// anothername = "dotcommujee";
+
+// console.log(myYoutubename)
+// console.log(anothername)
+
+let userOne = {
+    email:"mujeeb@gamil.com",
+    upi: "scdbksc"
+}
+let userTwo = userOne
+userTwo.email ="mujeeb703@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
