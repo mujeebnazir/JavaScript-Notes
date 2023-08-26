b6faf92a5219d571f0f7a6af5fca708d4c7baffb
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2023-08-26 10:31:25 +0000

    datatypes summary

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,9 @@
+//Primitive(call by value) :
+//7 types : String,Number,Boolean, null,undefined,symbol,BigInt
+
+
+
+//refrenceType(non primitive):
+//Array,Objects, Functions
+
+//js is dynamic not static language
