eaa75a4cdfe8932b82120296fa195d7698f9a0c5
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2023-08-26 16:42:25 +0000

    date

---
+++ 01-basics/Dates.js
@@ -0,0 +1,24 @@
+// //Dates
+ let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+console.log(myDate.getDate());
+console.log(myDate.getMonth);
+console.log(myDate.getDay());
+
+
+// console.log(typeof myDate)
+// let myCreatedDate = new Date(2023,0 ,23)
+// console.log(myCreatedDate.toDateString())
+
+let timeStamp = Date.now()
+console.log(Math.floor(timeStamp/1000));
+
+
+
+
